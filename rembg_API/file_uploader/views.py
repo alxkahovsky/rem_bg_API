7d0d1375c2e58b48dca8e0fce7ed1842a62686eb
EchoFoe <aspirpd@gmail.com>
2022-04-25 12:55:10 +0300

    #4 file uploader/views.py (add file response, after pocessing file)

--- rembg_API/file_uploader/views.py
+++ rembg_API/file_uploader/views.py
@@ -9,6 +9,8 @@ from PIL import Image
 import asyncio
 from asgiref.sync import sync_to_async
 import os
+from django.http import HttpResponse
+
 
 
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
@@ -16,11 +18,15 @@ os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 async def async_rembg(remark):
   file = await sync_to_async(File.objects.get, thread_sensitive=True)(remark=remark)
   input_path = file.get_os_path()
+  input_name = file.get_file_name()
   input = Image.open(input_path)
   output = remove(input)
   output.save(input_path, 'PNG')
   file.delete()
-  return (status.HTTP_201_CREATED)
+  return {'path': input_path,
+          'name': input_name,
+          }
+
 
 class FileView(APIView):
   parser_classes = (MultiPartParser, FormParser)
@@ -30,8 +36,14 @@ class FileView(APIView):
     if file_serializer.is_valid():
       cd = file_serializer.validated_data
       file_serializer.save()
-      rembg_status = str(asyncio.run(async_rembg(cd['remark'])))
-      return Response(file_serializer.data, status=rembg_status)
+      rembg_status = asyncio.run(async_rembg(cd['remark']))
+      path = rembg_status['path']
+      with open(path, 'rb') as image:
+        file = image.read()
+        response = HttpResponse(file, content_type='image/png')
+        filename = rembg_status['name']
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
     else:
       return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
